44a78a17044c486412374146e031f65403cf7269
James N. V. Cash <james.nvc@gmail.com>
2012-11-01 15:42:11 -0400

    Don't start URLConnection immediately, so it can be added to queue

--- Classes/Ejecta/EJUtils/EJBindingHttpRequest.m
+++ Classes/Ejecta/EJUtils/EJBindingHttpRequest.m
@@ -13,7 +13,7 @@
 	[requestHeaders release];
 	[self clearRequest];
 	[self clearConnection];
-	
+
 	[super dealloc];
 }
 
@@ -33,7 +33,7 @@
 
 - (NSString *)getResponseText {
 	if( !response || !responseBody ) { return NULL; }
-	
+
 	NSStringEncoding encoding = NSASCIIStringEncoding;
 	if ( response.textEncodingName ) {
 		CFStringEncoding cfEncoding = CFStringConvertIANACharSetNameToEncoding((CFStringRef) [response textEncodingName]);
@@ -63,7 +63,7 @@
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connectionp {
 	state = kEJHttpRequestStateDone;
-	
+
 	[connection release]; connection = NULL;
 	[self triggerEvent:@"load" argc:0 argv:NULL];
 	[self triggerEvent:@"loadend" argc:0 argv:NULL];
@@ -72,7 +72,7 @@
 
 - (void)connection:(NSURLConnection *)connectionp didFailWithError:(NSError *)error {
 	state = kEJHttpRequestStateDone;
-	
+
 	[connection release]; connection = NULL;
 	if( error.code == kCFURLErrorTimedOut ) {
 		[self triggerEvent:@"timeout" argc:0 argv:NULL];
@@ -86,7 +86,7 @@
 
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)responsep {
 	state = kEJHttpRequestStateHeadersReceived;
-	
+
 	[response release];
 	response = (NSHTTPURLResponse *)[responsep retain];
 	[self triggerEvent:@"progress" argc:0 argv:NULL];
@@ -95,7 +95,7 @@
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
 	state = kEJHttpRequestStateLoading;
-	
+
 	if( !responseBody ) {
 		responseBody = [[NSMutableData alloc] initWithCapacity:1024 * 10]; // 10kb
 	}
@@ -106,32 +106,32 @@
 
 
 
-EJ_BIND_FUNCTION(open, ctx, argc, argv) {	
+EJ_BIND_FUNCTION(open, ctx, argc, argv) {
 	if( argc < 2 ) { return NULL; }
-	
+
 	// Cleanup previous request, if any
 	[self clearConnection];
 	[self clearRequest];
-	
+
 	method = [JSValueToNSString( ctx, argv[0] ) retain];
 	url = [JSValueToNSString( ctx, argv[1] ) retain];
 	async = argc > 2 ? JSValueToBoolean( ctx, argv[2] ) : true;
-	
+
 	if( argc > 4 ) {
 		user = [JSValueToNSString( ctx, argv[3] ) retain];
 		password = [JSValueToNSString( ctx, argv[4] ) retain];
 	}
-	
+
 	state = kEJHttpRequestStateOpened;
 	return NULL;
 }
 
 EJ_BIND_FUNCTION(setRequestHeader, ctx, argc, argv) {
 	if( argc < 2 ) { return NULL; }
-	
+
 	NSString * header = JSValueToNSString( ctx, argv[0] );
 	NSString * value = JSValueToNSString( ctx, argv[1] );
-	
+
 	[requestHeaders setObject:value forKey:header];
 	return NULL;
 }
@@ -146,22 +146,22 @@ EJ_BIND_FUNCTION(abort, ctx, argc, argv) {
 
 EJ_BIND_FUNCTION(getAllResponseHeaders, ctx, argc, argv) {
 	if( !response ) { return NULL; }
-	
+
 	NSMutableString * headers = [NSMutableString string];
 	for( NSString * key in [response allHeaderFields] ) {
 		id value = [[response allHeaderFields] objectForKey:key];
 		[headers appendFormat:@"%@: %@\n", key, value];
 	}
-	
+
 	return NSStringToJSValue(ctx, headers);
 }
 
 EJ_BIND_FUNCTION(getResponseHeader, ctx, argc, argv) {
 	if( argc < 1 || !response ) { return NULL; }
-	
+
 	NSString * header = JSValueToNSString( ctx, argv[0] );
 	NSString * value = [[response allHeaderFields] objectForKey:header];
-	
+
 	return value ? NSStringToJSValue(ctx, value) : NULL;
 }
 
@@ -172,46 +172,48 @@ EJ_BIND_FUNCTION(overrideMimeType, ctx, argc, argv) {
 
 EJ_BIND_FUNCTION(send, ctx, argc, argv) {
 	if( !method || !url ) { return NULL; }
-	
+
 	[self clearConnection];
 
 	NSURL *remoteUrl = [NSURL URLWithString:url];
 	if (![remoteUrl host]) { // No host => local file
 		remoteUrl = [NSURL fileURLWithPath:[[EJApp instance] pathForResource:url]];
 	}
-	
+
 	NSMutableURLRequest * request = [[NSMutableURLRequest alloc] initWithURL:remoteUrl];
 	[request setHTTPMethod:method];
-	
+
 	for( NSString * header in requestHeaders ) {
 		[request setValue:[requestHeaders objectForKey:header] forHTTPHeaderField:header];
 	}
-	
+
 	if( argc > 0 ) {
 		NSString * requestBody = JSValueToNSString( ctx, argv[0] );
 		NSData * requestData = [NSData dataWithBytes:[requestBody UTF8String] length:[requestBody length]];
 		[request setHTTPBody:requestData];
 	}
-	
+
 	if( timeout ) {
 		NSTimeInterval timeoutSeconds = (float)timeout/1000.0f;
 		[request setTimeoutInterval:timeoutSeconds];
-	}	
-	
+	}
+
 	NSLog(@"XHR: %@ %@", method, url);
 	[self triggerEvent:@"loadstart" argc:0 argv:NULL];
-	
+
 	if( async ) {
 		state = kEJHttpRequestStateLoading;
-		connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+		connection = [[NSURLConnection alloc] initWithRequest:request
+		                                             delegate:self
+		                                     startImmediately:NO];
 		[connection setDelegateQueue:[EJApp instance].opQueue];
 		[connection start];
 	}
-	else {	
+	else {
 		NSData * data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
 		responseBody = [[NSMutableData alloc] initWithData:data];
 		[response retain];
-		
+
 		state = kEJHttpRequestStateDone;
 		if( response.statusCode == 200 ) {
 			[self triggerEvent:@"load" argc:0 argv:NULL];
@@ -220,7 +222,7 @@ EJ_BIND_FUNCTION(send, ctx, argc, argv) {
 		[self triggerEvent:@"readystatechange" argc:0 argv:NULL];
 	}
 	[request release];
-	
+
 	return NULL;
 }
 
@@ -231,7 +233,7 @@ EJ_BIND_GET(readyState, ctx) {
 EJ_BIND_GET(response, ctx) {
 	NSString * responseText = [self getResponseText];
 	if( !responseText ) { return NULL; }
-	
+
 	if( type == kEJHttpRequestTypeJSON ) {
 		JSStringRef jsText = JSStringCreateWithCFString((CFStringRef)responseText);
 		JSObjectRef jsonObject = (JSObjectRef)JSValueMakeFromJSONString(ctx, jsText);
@@ -244,7 +246,7 @@ EJ_BIND_GET(response, ctx) {
 }
 
 EJ_BIND_GET(responseText, ctx) {
-	NSString * responseText = [self getResponseText];	
+	NSString * responseText = [self getResponseText];
 	return responseText ? NSStringToJSValue( ctx, responseText ) : NULL;
 }
 
@@ -254,7 +256,7 @@ EJ_BIND_GET(status, ctx) {
 
 EJ_BIND_GET(statusText, ctx) {
 	if( !response ) { return NULL; }
-	
+
 	// FIXME: should be "200 OK" instead of just "200"
 	NSString * code = [NSString stringWithFormat:@"%d", response.statusCode];
 	return NSStringToJSValue(ctx, code);
